cec673388b24f5a469afa65d3f7acb2fe57c0e4e
Adam Cooke <adam@atechmedia.com>
2015-09-29 12:49:41 +0100

    Initial Commit

---
+++ MoonropeClient/MoonropeClient.h
@@ -0,0 +1,19 @@
+//
+//  MoonropeClient.h
+//  MoonropeClient
+//
+//  Created by Adam Cooke on 29/09/2015.
+//  Copyright © 2015 Adam Cooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoonropeClient.
+FOUNDATION_EXPORT double MoonropeClientVersionNumber;
+
+//! Project version string for MoonropeClient.
+FOUNDATION_EXPORT const unsigned char MoonropeClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoonropeClient/PublicHeader.h>
+
+
