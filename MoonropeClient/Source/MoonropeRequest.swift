5ae18dac22249c350ccb9745fd321ad11b177a02
Adam Cooke <me@adamcooke.io>
2017-02-15 16:07:59 +0000

    don't be printing things in random places

--- MoonropeClient/Source/MoonropeRequest.swift
+++ MoonropeClient/Source/MoonropeRequest.swift
@@ -67,7 +67,6 @@ public class MoonropeRequest {
                 
                 let errorWithCodeData : [String:Any?]?
                 if(errorType == "error") {
-                    print("error")
                     errorWithCodeData = errorData as? [String:Any?]
                 } else {
                     errorWithCodeData = nil
